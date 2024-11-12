ddbd453eadbb0e16b780d458877a3bf74a077a7e
aki <aki.akaguma@hotmail.com>
2024-11-12 20:49:36 +0900

    add benches

---
+++ benches/base64_bench1.rs
@@ -0,0 +1,20 @@
+use basexx::*;
+use criterion::*;
+
+mod utils;
+
+fn bench_base64_1(c: &mut Criterion) {
+    let v = utils::read_data_t1();
+    let base64 = Base64::default();
+    c.bench_function("base64_1", |b| b.iter(|| base64.encode(black_box(&v))));
+}
+
+fn bench_base64_2(c: &mut Criterion) {
+    let v = utils::read_data_t1();
+    let base64 = Base64::default();
+    let a = base64.encode(black_box(&v)).unwrap();
+    c.bench_function("base64_2", |b| b.iter(|| base64.decode(black_box(&a))));
+}
+
+criterion_group!(benches, bench_base64_1, bench_base64_2);
+criterion_main!(benches);
