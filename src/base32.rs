e76bc5186681cb9fc70b5aae282855cb49f34434
aki <aki.akaguma@hotmail.com>
2024-11-15 11:21:16 +0900

    add test: it_works_0 to base32, base64

--- src/base32.rs
+++ src/base32.rs
@@ -267,6 +267,16 @@ mod tests {
     use super::*;
 
     #[test]
+    fn it_works_0() {
+        let inp = [0u8, 0, 1, 1].to_vec();
+        let oup = "AAAACAI".to_string();
+        let ags = AsciiGraphicSet::with_slice(&_CMAP32);
+        let r1 = _encode_base32(&ags, &inp).unwrap();
+        assert_eq!(r1, oup);
+        let r2 = _decode_base32(&ags, &r1).unwrap();
+        assert_eq!(r2, inp);
+    }
+    #[test]
     fn it_works_1() {
         let inp = b"ABCDEFGHIJKL".to_vec();
         let oup = "IFBEGRCFIZDUQSKKJNGA".to_string();
