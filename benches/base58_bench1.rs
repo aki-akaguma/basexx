abffcbaa21c5e1e14b969ec29a40b643b47e60fa
aki <aki.akaguma@hotmail.com>
2024-12-01 20:00:53 +0900

    rename: bench _enc, _dec

--- benches/base58_bench1.rs
+++ benches/base58_bench1.rs
@@ -3,18 +3,18 @@ use criterion::*;
 
 mod utils;
 
-fn bench_base58_1(c: &mut Criterion) {
+fn bench_base58_enc(c: &mut Criterion) {
     let v = utils::read_data_t1();
     let base58 = Base58::default();
-    c.bench_function("base58_1", |b| b.iter(|| base58.encode(black_box(&v))));
+    c.bench_function("base58_enc", |b| b.iter(|| base58.encode(black_box(&v))));
 }
 
-fn bench_base58_2(c: &mut Criterion) {
+fn bench_base58_dec(c: &mut Criterion) {
     let v = utils::read_data_t1();
     let base58 = Base58::default();
     let a = base58.encode(black_box(&v)).unwrap();
-    c.bench_function("base58_2", |b| b.iter(|| base58.decode(black_box(&a))));
+    c.bench_function("base58_dec", |b| b.iter(|| base58.decode(black_box(&a))));
 }
 
-criterion_group!(benches, bench_base58_1, bench_base58_2);
+criterion_group!(benches, bench_base58_enc, bench_base58_dec);
 criterion_main!(benches);
--- benches/base64_bench1.rs
+++ benches/base64_bench1.rs
@@ -3,18 +3,18 @@ use criterion::*;
 
 mod utils;
 
-fn bench_base64_1(c: &mut Criterion) {
+fn bench_base64_enc(c: &mut Criterion) {
     let v = utils::read_data_t1();
     let base64 = Base64::default();
-    c.bench_function("base64_1", |b| b.iter(|| base64.encode(black_box(&v))));
+    c.bench_function("base64_enc", |b| b.iter(|| base64.encode(black_box(&v))));
 }
 
-fn bench_base64_2(c: &mut Criterion) {
+fn bench_base64_dec(c: &mut Criterion) {
     let v = utils::read_data_t1();
     let base64 = Base64::default();
     let a = base64.encode(black_box(&v)).unwrap();
-    c.bench_function("base64_2", |b| b.iter(|| base64.decode(black_box(&a))));
+    c.bench_function("base64_dec", |b| b.iter(|| base64.decode(black_box(&a))));
 }
 
-criterion_group!(benches, bench_base64_1, bench_base64_2);
+criterion_group!(benches, bench_base64_enc, bench_base64_dec);
 criterion_main!(benches);
