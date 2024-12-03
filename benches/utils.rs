bd57e42f648988993eec8b3fa18e19780ce9fc4b
aki <aki.akaguma@hotmail.com>
2024-12-03 23:34:25 +0900

    added benches: ascii

--- benches/utils.rs
+++ benches/utils.rs
@@ -1,8 +1,20 @@
 use std::io::Read;
 
+#[allow(dead_code)]
 pub fn read_data_t1() -> Vec<u8> {
     let mut v = Vec::new();
     let mut f = std::fs::File::open("fixtures/t4.data").unwrap();
     let _ = f.read_to_end(&mut v);
+    v
+}
+
+#[allow(dead_code)]
+pub fn read_ascii_t1() -> Vec<u8> {
+    let mut v = Vec::new();
+    let mut f = std::fs::File::open("fixtures/t4.base64.ascii").unwrap();
+    let _ = f.read_to_end(&mut v);
+    if v[v.len() - 1] == b'\n' {
+        v.pop();
+    };
     v
 }
