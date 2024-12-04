653c645517b99ad046ed97c9f3a1188ebfb2abff
aki <aki.akaguma@hotmail.com>
2024-12-04 17:54:47 +0900

    2nd commit of sse2

--- src/base64.rs
+++ src/base64.rs
@@ -36,14 +36,18 @@ impl Base64 {
 
 impl Base64 {
     pub fn encode(&self, a: &[u8]) -> Result<String, EncodeError> {
-        _encode_base64(&self.ags, a)
         /*
-        if cfg!(target_feature = "sse2") {
-            unsafe { _encode_base64_sse2(&self.ags, a) }
-        } else {
-            _encode_base64(&self.ags, a)
-        }
+        _encode_base64_scalar(&self.ags, a)
         */
+        if cfg!(target_feature = "sse2") {
+            if is_x86_feature_detected!("ssse3") {
+                unsafe { _encode_base64_ssse3(&self.ags, a) }
+            } else {
+                _encode_base64_scalar(&self.ags, a)
+            }
+        } else {
+            _encode_base64_scalar(&self.ags, a)
+        }
     }
     pub fn decode(&self, a: &str) -> Result<Vec<u8>, DecodeError> {
         _decode_base64(&self.ags, a)
@@ -60,8 +64,8 @@ impl Base64 {
 */
 // http://0x80.pl/notesen/2016-01-12-sse-base64-encoding.html
 #[inline(never)]
-#[target_feature(enable = "sse2")]
-unsafe fn _encode_base64_sse2(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String, EncodeError> {
+#[target_feature(enable = "ssse3")]
+unsafe fn _encode_base64_ssse3(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String, EncodeError> {
     //panic!("PASS: SSE2");
     #[cfg(target_arch = "x86")]
     use core::arch::x86::*;
@@ -76,7 +80,7 @@ unsafe fn _encode_base64_sse2(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String,
     let mut a_ptr = a.as_ptr();
     let mut a_out = r.as_ptr();
     let end_ptr = unsafe { a_ptr.add(a_len) };
-    let end_ptr_limit = unsafe { end_ptr.sub(16) };
+    let end_ptr_limit = unsafe { end_ptr.sub(16 - 1) };
     //
     let mm_shuf: __m128i = _mm_set_epi8(10, 11, 9, 10, 7, 8, 6, 7, 4, 5, 3, 4, 1, 2, 0, 1);
     let mm_t0_set1 = _mm_set1_epi32(0x0fc0fc00);
@@ -86,7 +90,7 @@ unsafe fn _encode_base64_sse2(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String,
     //
     while a_ptr < end_ptr_limit {
         let mm_in = _mm_loadu_si128(a_ptr as *const __m128i);
-        let mm_in = _mm_shuffle_epi8(mm_in, mm_shuf);
+        let mm_in = _mm_shuffle_epi8(mm_in, mm_shuf); // over ssse3
         let mm_t0 = _mm_and_si128(mm_in, mm_t0_set1);
         let mm_t1 = _mm_mulhi_epu16(mm_t0, mm_t1_set1);
         let mm_t2 = _mm_and_si128(mm_in, mm_t2_set1);
@@ -95,6 +99,7 @@ unsafe fn _encode_base64_sse2(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String,
         _mm_storeu_si128(a_out as *mut __m128i, indices);
         a_out = unsafe { a_out.add(16) };
         a_ptr = unsafe { a_ptr.add(4 * 3) };
+        _mm_prefetch(a_ptr.cast::<i8>(), _MM_HINT_T0);
     }
     //
     r_idx = unsafe { a_out.offset_from(r.as_ptr()) as usize };
@@ -156,12 +161,44 @@ unsafe fn _encode_base64_sse2(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String,
     assert!(s.len() == r.len());
     Ok(s)
 }
+
 #[inline(never)]
-fn _encode_base64(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String, EncodeError> {
+fn _encode_base64_scalar(ags: &AsciiGraphicSet, a: &[u8]) -> Result<String, EncodeError> {
     let rsz = 1 + ((a.len() + 2) / 3) * 4;
     // encode binary
     let mut r = vec![0u8; rsz];
     let mut r_idx = 0;
+    //
+    let mut iter = a.chunks_exact(6);
+    let mut nx = iter.next();
+    while let Some(aa) = nx {
+        let b0 = aa[0];
+        let b1 = aa[1];
+        let b2 = aa[2];
+        let b3 = aa[3];
+        let b4 = aa[4];
+        let b5 = aa[5];
+        let v0 = b0 >> 2;
+        let v1 = (b0 & 0b11) << 4 | (b1 >> 4);
+        let v2 = (b1 & 0b1111) << 2 | (b2 >> 6);
+        let v3 = b2 & 0b111111;
+        let v4 = b3 >> 2;
+        let v5 = (b3 & 0b11) << 4 | (b4 >> 4);
+        let v6 = (b4 & 0b1111) << 2 | (b5 >> 6);
+        let v7 = b5 & 0b111111;
+        r[r_idx] = v0;
+        r[r_idx + 1] = v1;
+        r[r_idx + 2] = v2;
+        r[r_idx + 3] = v3;
+        r[r_idx + 4] = v4;
+        r[r_idx + 5] = v5;
+        r[r_idx + 6] = v6;
+        r[r_idx + 7] = v7;
+        r_idx += 8;
+        nx = iter.next();
+    }
+    let a = iter.remainder();
+    //
     let mut iter = a.chunks_exact(3);
     let mut nx = iter.next();
     while let Some(aa) = nx {
@@ -289,7 +326,7 @@ mod tests {
         let inp = [0u8, 0, 1, 1].to_vec();
         let oup = "AAABAQ".to_string();
         let ags = AsciiGraphicSet::with_slice(&_CMAP64);
-        let r1 = _encode_base64(&ags, &inp).unwrap();
+        let r1 = _encode_base64_scalar(&ags, &inp).unwrap();
         assert_eq!(r1, oup);
         let r2 = _decode_base64(&ags, &r1).unwrap();
         assert_eq!(r2, inp);
@@ -299,7 +336,7 @@ mod tests {
         let inp = b"ABCDEFGHIJKL".to_vec();
         let oup = "QUJDREVGR0hJSktM".to_string();
         let ags = AsciiGraphicSet::with_slice(&_CMAP64);
-        let r1 = _encode_base64(&ags, &inp).unwrap();
+        let r1 = _encode_base64_scalar(&ags, &inp).unwrap();
         assert_eq!(r1, oup);
         let r2 = _decode_base64(&ags, &r1).unwrap();
         assert_eq!(r2, inp);
@@ -309,7 +346,7 @@ mod tests {
         let inp = b"ABCDEFGHIJK".to_vec();
         let oup = "QUJDREVGR0hJSks".to_string();
         let ags = AsciiGraphicSet::with_slice(&_CMAP64);
-        let r1 = _encode_base64(&ags, &inp).unwrap();
+        let r1 = _encode_base64_scalar(&ags, &inp).unwrap();
         assert_eq!(r1, oup);
         let r2 = _decode_base64(&ags, &r1).unwrap();
         assert_eq!(r2, inp);
@@ -319,7 +356,7 @@ mod tests {
         let inp = b"ABCDEFGHIJ".to_vec();
         let oup = "QUJDREVGR0hJSg".to_string();
         let ags = AsciiGraphicSet::with_slice(&_CMAP64);
-        let r1 = _encode_base64(&ags, &inp).unwrap();
+        let r1 = _encode_base64_scalar(&ags, &inp).unwrap();
         assert_eq!(r1, oup);
         let r2 = _decode_base64(&ags, &r1).unwrap();
         assert_eq!(r2, inp);
