d2e5a949de6b1291d20f4dc717ec4e41b7d09c56
aki <aki.akaguma@hotmail.com>
2024-12-06 20:09:05 +0900

    fixed bug: case of no ssse3: base64 benches tests

--- src/base64/base64_ssse3/benches.rs
+++ src/base64/base64_ssse3/benches.rs
@@ -4,6 +4,9 @@ use criterion::*;
 
 #[allow(dead_code)]
 pub fn bench_base64_ssse3_enc(c: &mut Criterion) {
+    if !is_x86_feature_detected!("ssse3") {
+        return;
+    }
     let v = test_utils::read_t4_data();
     let ags = AsciiGraphicSet::with_slice(&test_utils::_CMAP64);
     c.bench_function("base64_ssse3_enc", |b| {
@@ -13,6 +16,9 @@ pub fn bench_base64_ssse3_enc(c: &mut Criterion) {
 
 #[allow(dead_code)]
 pub fn bench_base64_ssse3_dec(c: &mut Criterion) {
+    if !is_x86_feature_detected!("ssse3") {
+        return;
+    }
     let v = test_utils::read_t4_data();
     let ags = AsciiGraphicSet::with_slice(&test_utils::_CMAP64);
     let a = unsafe { _encode_base64_ssse3(black_box(&ags), black_box(&v)).unwrap() };
