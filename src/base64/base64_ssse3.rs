ce5c91ee60929c3ebfc3b1370e323203970f207b
aki <aki.akaguma@hotmail.com>
2024-12-08 00:26:45 +0000

    updated base64_ssse3

--- src/base64/base64_ssse3.rs
+++ src/base64/base64_ssse3.rs
@@ -6,102 +6,89 @@ use super::super::*;
 #[target_feature(enable = "ssse3")]
 pub(crate) unsafe fn _encode_base64_ssse3(
     ags: &AsciiGraphicSet,
-    a: &[u8],
+    inp: &[u8],
 ) -> Result<String, EncodeError> {
+    let oup_sz = 1 + ((inp.len() + 2) / 3) * 4;
+    let mut oup = vec![0u8; oup_sz];
+    let oup_idx = _encode_base64_ssse3_chunks12(ags, inp, &mut oup[0..])?;
+    oup.resize(oup_idx, 0u8);
+    let string = unsafe { String::from_utf8_unchecked(oup) };
+    assert!(string.len() == oup_idx);
+    Ok(string)
+}
+
+#[inline(always)]
+pub(crate) unsafe fn _encode_base64_ssse3_chunks12(
+    ags: &AsciiGraphicSet,
+    inp: &[u8],
+    oup: &mut [u8],
+) -> Result<usize, EncodeError> {
     //panic!("PASS: SSE2");
     #[cfg(target_arch = "x86")]
     use core::arch::x86::*;
     #[cfg(target_arch = "x86_64")]
     use core::arch::x86_64::*;
     //
-    let rsz = 1 + ((a.len() + 2) / 3) * 4;
-    // encode binary
-    let mut r = vec![0u8; rsz];
-    let mut r_idx;
-    let a_len = a.len();
-    let mut a_ptr = a.as_ptr();
-    let mut a_out = r.as_ptr();
-    let end_ptr = unsafe { a_ptr.add(a_len) };
-    let end_ptr_limit = unsafe { end_ptr.sub(16 - 1) };
-    //
-    let mm_shuf: __m128i = _mm_set_epi8(10, 11, 9, 10, 7, 8, 6, 7, 4, 5, 3, 4, 1, 2, 0, 1);
-    let mm_t0_set1 = _mm_set1_epi32(0x0fc0fc00);
-    let mm_t1_set1 = _mm_set1_epi32(0x04000040);
-    let mm_t2_set1 = _mm_set1_epi32(0x003f03f0);
-    let mm_t3_set1 = _mm_set1_epi32(0x01000010);
-    //
-    while a_ptr < end_ptr_limit {
-        let mm_in = _mm_loadu_si128(a_ptr as *const __m128i);
-        let mm_in = _mm_shuffle_epi8(mm_in, mm_shuf); // over ssse3
-        let mm_t0 = _mm_and_si128(mm_in, mm_t0_set1);
-        let mm_t1 = _mm_mulhi_epu16(mm_t0, mm_t1_set1);
-        let mm_t2 = _mm_and_si128(mm_in, mm_t2_set1);
-        let mm_t3 = _mm_mullo_epi16(mm_t2, mm_t3_set1);
-        let indices = _mm_or_si128(mm_t1, mm_t3);
-        _mm_storeu_si128(a_out as *mut __m128i, indices);
-        a_out = unsafe { a_out.add(16) };
-        a_ptr = unsafe { a_ptr.add(4 * 3) };
-        _mm_prefetch(a_ptr.cast::<i8>(), _MM_HINT_T0);
-    }
-    //
-    r_idx = unsafe { a_out.offset_from(r.as_ptr()) as usize };
-    let new_a_len = unsafe { end_ptr.offset_from(a_ptr) as usize };
-    if new_a_len > 0 {
-        let a = unsafe { std::slice::from_raw_parts(a_ptr, new_a_len) };
+    let mut oup_idx = 0;
+    let inp = if inp.len() < 16 {
+        inp
+    } else {
+        let inp_len = inp.len();
+        let mut inp_ptr = inp.as_ptr();
+        let mut oup_ptr = oup.as_mut_ptr();
+        let end_ptr = unsafe { inp_ptr.add(inp_len) };
+        let end_ptr_limit = unsafe { end_ptr.sub(16 - 1) };
         //
-        let mut iter = a.chunks_exact(3);
-        let mut nx = iter.next();
-        while let Some(aa) = nx {
-            let b0 = aa[0];
-            let b1 = aa[1];
-            let b2 = aa[2];
-            let v0 = b0 >> 2;
-            let v1 = (b0 & 0b11) << 4 | (b1 >> 4);
-            let v2 = (b1 & 0b1111) << 2 | (b2 >> 6);
-            let v3 = b2 & 0b111111;
-            r[r_idx] = v0;
-            r[r_idx + 1] = v1;
-            r[r_idx + 2] = v2;
-            r[r_idx + 3] = v3;
-            r_idx += 4;
-            nx = iter.next();
+        let mm_shuf: __m128i = _mm_set_epi8(10, 11, 9, 10, 7, 8, 6, 7, 4, 5, 3, 4, 1, 2, 0, 1);
+        let mm_t0_set1 = _mm_set1_epi32(0x0fc0fc00);
+        let mm_t1_set1 = _mm_set1_epi32(0x04000040);
+        let mm_t2_set1 = _mm_set1_epi32(0x003f03f0);
+        let mm_t3_set1 = _mm_set1_epi32(0x01000010);
+        let mut cc16 = [0u64; 2];
+        //
+        while inp_ptr < end_ptr_limit {
+            let mm_in = _mm_loadu_si128(inp_ptr as *const __m128i);
+            let mm_in = _mm_shuffle_epi8(mm_in, mm_shuf); // ssse3
+            let mm_t0 = _mm_and_si128(mm_in, mm_t0_set1);
+            let mm_t1 = _mm_mulhi_epu16(mm_t0, mm_t1_set1);
+            let mm_t2 = _mm_and_si128(mm_in, mm_t2_set1);
+            let mm_t3 = _mm_mullo_epi16(mm_t2, mm_t3_set1);
+            let indices = _mm_or_si128(mm_t1, mm_t3);
+            _mm_storeu_si128(cc16.as_mut_ptr() as *mut __m128i, indices);
+            //
+            let c16 = unsafe { std::slice::from_raw_parts_mut(oup_ptr, 16) };
+            oup_ptr = unsafe { oup_ptr.add(16) };
+            inp_ptr = unsafe { inp_ptr.add(4 * 3) };
+            //_mm_prefetch(inp_ptr.cast::<i8>(), _MM_HINT_T0);
+            //
+            // from binary to ascii
+            //ags.binary_to_ascii(c16)?;
+            c16[0] = ags.getq((cc16[0] & 0xFF) as u8)?;
+            c16[1] = ags.getq(((cc16[0] >> 8) & 0xFF) as u8)?;
+            c16[2] = ags.getq(((cc16[0] >> 2 * 8) & 0xFF) as u8)?;
+            c16[3] = ags.getq(((cc16[0] >> 3 * 8) & 0xFF) as u8)?;
+            c16[4] = ags.getq(((cc16[0] >> 4 * 8) & 0xFF) as u8)?;
+            c16[5] = ags.getq(((cc16[0] >> 5 * 8) & 0xFF) as u8)?;
+            c16[6] = ags.getq(((cc16[0] >> 6 * 8) & 0xFF) as u8)?;
+            c16[7] = ags.getq(((cc16[0] >> 7 * 8) & 0xFF) as u8)?;
+            //
+            c16[8] = ags.getq(((cc16[1]) & 0xFF) as u8)?;
+            c16[9] = ags.getq(((cc16[1] >> 8) & 0xFF) as u8)?;
+            c16[10] = ags.getq(((cc16[1] >> 2 * 8) & 0xFF) as u8)?;
+            c16[11] = ags.getq(((cc16[1] >> 3 * 8) & 0xFF) as u8)?;
+            c16[12] = ags.getq(((cc16[1] >> 4 * 8) & 0xFF) as u8)?;
+            c16[13] = ags.getq(((cc16[1] >> 5 * 8) & 0xFF) as u8)?;
+            c16[14] = ags.getq(((cc16[1] >> 6 * 8) & 0xFF) as u8)?;
+            c16[15] = ags.getq(((cc16[1] >> 7 * 8) & 0xFF) as u8)?;
         }
-        let aa = iter.remainder();
-        match aa.len() {
-            0 => (),
-            1 => {
-                let b0 = aa[0];
-                let b1 = 0;
-                let v0 = b0 >> 2;
-                let v1 = (b0 & 0b11) << 4 | (b1 >> 4);
-                r[r_idx] = v0;
-                r[r_idx + 1] = v1;
-                r_idx += 2;
-            }
-            2 => {
-                let b0 = aa[0];
-                let b1 = aa[1];
-                let b2 = 0;
-                let v0 = b0 >> 2;
-                let v1 = (b0 & 0b11) << 4 | (b1 >> 4);
-                let v2 = (b1 & 0b1111) << 2 | (b2 >> 6);
-                r[r_idx] = v0;
-                r[r_idx + 1] = v1;
-                r[r_idx + 2] = v2;
-                r_idx += 3;
-            }
-            _ => unreachable!(),
-        }
-    }
-    r.resize(r_idx, 0u8);
-    // from binary to ascii
-    match ags.binary_to_ascii(&mut r) {
-        Ok(()) => (),
-        Err(err) => return Err(err),
-    }
-    let s = String::from_utf8_lossy(&r).to_string();
-    assert!(s.len() == r.len());
-    Ok(s)
+        oup_idx = unsafe { oup_ptr.offset_from(oup.as_ptr()) as usize };
+        let new_inp_len = unsafe { end_ptr.offset_from(inp_ptr) as usize };
+        let remaind = unsafe { std::slice::from_raw_parts(inp_ptr, new_inp_len) };
+        remaind
+    };
+    //
+    let oo_idx = _encode_base64_scalar_chunks6(ags, inp, &mut oup[oup_idx..])?;
+    Ok(oup_idx + oo_idx)
 }
 
 #[inline(never)]
@@ -110,59 +97,108 @@ pub(crate) unsafe fn _decode_base64_ssse3(
     ags: &AsciiGraphicSet,
     a: &str,
 ) -> Result<Vec<u8>, DecodeError> {
-    // from ascii to binary
-    let mut r = a.as_bytes().to_vec();
-    match ags.ascii_to_binary(&mut r) {
-        Ok(()) => (),
-        Err(err) => return Err(err),
-    }
-    // decode binary
-    let rsz = (r.len() / 4) * 3 + 2;
-    //let mut rr = Vec::new();
-    let mut rr = vec![0u8; rsz];
-    let mut r_idx = 0;
-    let mut iter = r.chunks_exact(4);
-    let mut nx = iter.next();
-    while let Some(aa) = nx {
-        let c0 = aa[0];
-        let c1 = aa[1];
-        let c2 = aa[2];
-        let c3 = aa[3];
-        let v0 = (c0 << 2) | (c1 >> 4);
-        let v1 = (c1 << 4) | (c2 >> 2);
-        let v2 = (c2 << 6) | c3;
-        rr[r_idx] = v0;
-        rr[r_idx + 1] = v1;
-        rr[r_idx + 2] = v2;
-        r_idx += 3;
-        nx = iter.next();
-    }
-    let aa = iter.remainder();
-    match aa.len() {
-        0 => (),
-        2 => {
-            let c0 = aa[0];
-            let c1 = aa[1];
-            let v0 = (c0 << 2) | (c1 >> 4);
-            assert!(0b1111 & c1 == 0);
-            rr[r_idx] = v0;
-            r_idx += 1;
+    let inp = a.as_bytes();
+    let oup_sz = (inp.len() / 4) * 3 + 2;
+    let mut oup = vec![0u8; oup_sz];
+    let oup_idx = _decode_base64_ssse3_chunks16(ags, &inp, &mut oup[0..])?;
+    oup.resize(oup_idx, 0u8);
+    Ok(oup)
+}
+
+#[inline(always)]
+pub(crate) unsafe fn _decode_base64_ssse3_chunks16(
+    ags: &AsciiGraphicSet,
+    inp: &[u8],
+    oup: &mut [u8],
+) -> Result<usize, DecodeError> {
+    //panic!("PASS: SSE2");
+    #[cfg(target_arch = "x86")]
+    use core::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use core::arch::x86_64::*;
+    //
+    let mut oup_idx = 0;
+    let inp = if inp.len() < 16 {
+        inp
+    } else {
+        let inp_len = inp.len();
+        let mut inp_ptr = inp.as_ptr();
+        let mut oup_ptr = oup.as_mut_ptr();
+        let end_ptr = unsafe { inp_ptr.add(inp_len) };
+        let end_ptr_limit = unsafe { end_ptr.sub(16 - 1) };
+        //
+        let mm_shuf: __m128i =
+            _mm_setr_epi8(2, 1, 0, 6, 5, 4, 10, 9, 8, 14, 13, 12, -1, -1, -1, -1);
+        let mm_t0_set1 = _mm_set1_epi32(0x01400140);
+        let mm_t1_set1 = _mm_set1_epi32(0x00011000);
+        let mut cc16 = [0u64; 2];
+        //
+        while inp_ptr < end_ptr_limit {
+            // from ascii to binary
+            /*
+            let a16 = unsafe { std::slice::from_raw_parts(inp_ptr, 16) };
+            cc16[0] = ags.ascii_to_binary_c8(&a16[..8])?;
+            cc16[1] = ags.ascii_to_binary_c8(&a16[8..])?;
+            */
+            // from ascii to binary
+            let a16 = unsafe { std::slice::from_raw_parts(inp_ptr, 16) };
+            //
+            let c0 = ags.posq(a16[0])?;
+            let c1 = ags.posq(a16[1])?;
+            let c2 = ags.posq(a16[2])?;
+            let c3 = ags.posq(a16[3])?;
+            let c4 = ags.posq(a16[4])?;
+            let c5 = ags.posq(a16[5])?;
+            let c6 = ags.posq(a16[6])?;
+            let c7 = ags.posq(a16[7])?;
+            let cl = c7 as u64;
+            let cl = cl << 8 | c6 as u64;
+            let cl = cl << 8 | c5 as u64;
+            let cl = cl << 8 | c4 as u64;
+            let cl = cl << 8 | c3 as u64;
+            let cl = cl << 8 | c2 as u64;
+            let cl = cl << 8 | c1 as u64;
+            let cl = cl << 8 | c0 as u64;
+            cc16[0] = cl;
+            //
+            let c8 = ags.posq(a16[8])?;
+            let c9 = ags.posq(a16[9])?;
+            let c10 = ags.posq(a16[10])?;
+            let c11 = ags.posq(a16[11])?;
+            let c12 = ags.posq(a16[12])?;
+            let c13 = ags.posq(a16[13])?;
+            let c14 = ags.posq(a16[14])?;
+            let c15 = ags.posq(a16[15])?;
+            let cu = c15 as u64;
+            let cu = cu << 8 | c14 as u64;
+            let cu = cu << 8 | c13 as u64;
+            let cu = cu << 8 | c12 as u64;
+            let cu = cu << 8 | c11 as u64;
+            let cu = cu << 8 | c10 as u64;
+            let cu = cu << 8 | c9 as u64;
+            let cu = cu << 8 | c8 as u64;
+            cc16[1] = cu;
+            /*
+             */
+            //
+            let mm_in = _mm_loadu_si128(cc16.as_ptr() as *const __m128i);
+            let mm_t0 = _mm_maddubs_epi16(mm_in, mm_t0_set1); // ssse3
+            let mm_t1 = _mm_madd_epi16(mm_t0, mm_t1_set1);
+            let mm_values = _mm_shuffle_epi8(mm_t1, mm_shuf); // ssse3
+            _mm_storeu_si128(oup_ptr as *mut __m128i, mm_values);
+            //
+            oup_ptr = unsafe { oup_ptr.add(12) };
+            inp_ptr = unsafe { inp_ptr.add(16) };
+            _mm_prefetch(inp_ptr.cast::<i8>(), _MM_HINT_T0);
         }
-        3 => {
-            let c0 = aa[0];
-            let c1 = aa[1];
-            let c2 = aa[2];
-            let v0 = (c0 << 2) | (c1 >> 4);
-            let v1 = (c1 << 4) | (c2 >> 2);
-            assert!(0b11 & c2 == 0);
-            rr[r_idx] = v0;
-            rr[r_idx + 1] = v1;
-            r_idx += 2;
-        }
-        _ => return Err(DecodeError::InvalidLength(a.len())),
-    }
-    rr.resize(r_idx, 0u8);
-    Ok(rr)
+        oup_idx = unsafe { oup_ptr.offset_from(oup.as_ptr()) as usize };
+        let new_inp_len = unsafe { end_ptr.offset_from(inp_ptr) as usize };
+        let remaind = unsafe { std::slice::from_raw_parts(inp_ptr, new_inp_len) };
+        remaind
+    };
+    //
+    let oo_idx = _decode_base64_scalar_chunks4(ags, inp, &mut oup[oup_idx..])?;
+    Ok(oup_idx + oo_idx)
 }
 
 #[cfg(all(test, not(feature = "bench")))]
--- src/base64/base64_ssse3/tests.rs
+++ src/base64/base64_ssse3/tests.rs
@@ -55,3 +55,88 @@ fn it_works_3() {
     let r2 = unsafe { _decode_base64_ssse3(&ags, &r1).unwrap() };
     assert_eq!(r2, inp);
 }
+
+#[test]
+fn test_base64_ssse3_file_t4_enc() {
+    if !is_x86_feature_detected!("ssse3") {
+        return;
+    }
+    let inp = test_utils::read_t4_data();
+    let oup = test_utils::read_t4_base64_ascii();
+    let ags = AsciiGraphicSet::with_slice(&_CMAP64);
+    let r1 = unsafe { _encode_base64_ssse3(&ags, &inp).unwrap() };
+    assert_eq!(r1, oup);
+}
+
+#[test]
+fn test_base64_ssse3_file_t4_dec() {
+    if !is_x86_feature_detected!("ssse3") {
+        return;
+    }
+    let inp = test_utils::read_t4_base64_ascii();
+    let oup = test_utils::read_t4_data();
+    let ags = AsciiGraphicSet::with_slice(&_CMAP64);
+    let r2 = unsafe { _decode_base64_ssse3(&ags, &inp).unwrap() };
+    assert_eq!(r2, oup);
+}
+
+use rstest::rstest;
+use rstest_reuse::{self, *};
+
+#[template]
+#[rstest]
+//
+#[case(b"ABCDEFGHIJKL", "QUJDREVGR0hJSktM")]
+#[case(b"abcdefghijkl", "YWJjZGVmZ2hpamts")]
+#[case(b"0123456789+/", "MDEyMzQ1Njc4OSsv")]
+#[case(&[0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "AAECAwQFBgcICQoL")]
+#[case(&[244u8, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255], "9PX29/j5+vv8/f7/")]
+//
+#[case(b"ABCDEFGHIJK", "QUJDREVGR0hJSks")]
+#[case(b"abcdefghijk", "YWJjZGVmZ2hpams")]
+#[case(b"0123456789+", "MDEyMzQ1Njc4OSs")]
+#[case(&[0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "AAECAwQFBgcICQo")]
+#[case(&[244u8, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254], "9PX29/j5+vv8/f4")]
+//
+#[case(b"ABCDEFGHIJ", "QUJDREVGR0hJSg")]
+#[case(b"abcdefghij", "YWJjZGVmZ2hpag")]
+#[case(b"0123456789", "MDEyMzQ1Njc4OQ")]
+#[case(&[0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9], "AAECAwQFBgcICQ")]
+#[case(&[244u8, 245, 246, 247, 248, 249, 250, 251, 252, 253], "9PX29/j5+vv8/Q")]
+//
+#[case(&[0u8, 1], "AAE")]
+#[case(&[0u8,0, 1], "AAAB")]
+#[case(&[0u8,0,0, 1], "AAAAAQ")]
+#[case(&[0u8,0,0,0, 1], "AAAAAAE")]
+#[case(&[0u8,0,0,0,0, 1], "AAAAAAAB")]
+//
+#[case(&[0u8,0,0,0,0,0, 1], "AAAAAAAAAQ")]
+#[case(&[0u8,0,0,0,0,0,0, 1], "AAAAAAAAAAE")]
+#[case(&[0u8,0,0,0,0,0,0,0, 1], "AAAAAAAAAAAB")]
+#[case(&[0u8,0,0,0,0,0,0,0,0, 1], "AAAAAAAAAAAAAQ")]
+#[case(&[0u8,0,0,0,0,0,0,0,0,0, 1], "AAAAAAAAAAAAAAE")]
+fn two_simple_case_1(#[case] input: &[u8], #[case] output: &str) {}
+
+#[apply(two_simple_case_1)]
+fn base64_ssse3_encode_test(#[case] input: &[u8], #[case] output: &str) {
+    if !is_x86_feature_detected!("ssse3") {
+        return;
+    }
+    let inp = input.to_vec();
+    let oup = output.to_string();
+    let ags = AsciiGraphicSet::with_slice(&_CMAP64);
+    let r1 = unsafe { _encode_base64_ssse3(&ags, &inp).unwrap() };
+    assert_eq!(r1, oup);
+}
+
+#[apply(two_simple_case_1)]
+fn base64_ssse3_decode_test(#[case] output: &[u8], #[case] input: &str) {
+    if !is_x86_feature_detected!("ssse3") {
+        return;
+    }
+    let inp = input.to_string();
+    let oup = output.to_vec();
+    let ags = AsciiGraphicSet::with_slice(&_CMAP64);
+    let r2 = unsafe { _decode_base64_ssse3(&ags, &inp).unwrap() };
+    assert_eq!(r2, oup);
+}
