5ec6ed6e41a0ae71634016abda8c58ebc9b495dc
aki <aki.akaguma@hotmail.com>
2024-12-19 11:44:31 +0000

    added: ags 32,64 avx2

---
+++ src/ags/ags_32_avx2.rs
@@ -0,0 +1,138 @@
+use super::super::*;
+
+#[inline(never)]
+#[target_feature(enable = "avx2")]
+pub(crate) unsafe fn _binary_to_ascii_32_avx2(
+    lup: &[u8],
+    buf: &mut [u8],
+) -> Result<(), EncodeError> {
+    assert_eq!(lup.len(), 32);
+    _binary_to_ascii_32_avx2_chunks32(lup, buf)?;
+    Ok(())
+}
+
+#[inline(always)]
+pub(crate) unsafe fn _binary_to_ascii_32_avx2_chunks32(
+    lup: &[u8],
+    buf: &mut [u8],
+) -> Result<(), EncodeError> {
+    let buf = if buf.len() < 32 {
+        buf
+    } else {
+        let buf_len = buf.len();
+        let mut buf_ptr = buf.as_mut_ptr();
+        let end_ptr = unsafe { buf_ptr.add(buf_len) };
+        let end_ptr_limit = unsafe { end_ptr.sub(32 - 1) };
+        //
+        while buf_ptr < end_ptr_limit {
+            let mut buf2 = [0u64; 4];
+            buf2.copy_from_slice(std::slice::from_raw_parts(buf_ptr as *const u64, 4));
+            _binary_to_ascii_32_avx2_c32_chunks32(lup, &mut buf2)?;
+            *(buf_ptr as *mut u64) = buf2[0];
+            *((buf_ptr as *mut u64).add(1)) = buf2[1];
+            *((buf_ptr as *mut u64).add(2)) = buf2[2];
+            *((buf_ptr as *mut u64).add(3)) = buf2[3];
+            //
+            buf_ptr = unsafe { buf_ptr.add(32) };
+        }
+        let new_buf_len = unsafe { end_ptr.offset_from(buf_ptr) as usize };
+        let remaind = unsafe { std::slice::from_raw_parts_mut(buf_ptr, new_buf_len) };
+        remaind
+    };
+    _binary_to_ascii_scalar(lup, buf)?;
+    //
+    Ok(())
+}
+
+#[cfg(target_feature = "sse2")]
+#[inline(always)]
+//#[target_feature(enable = "avx2")]
+pub(crate) unsafe fn _binary_to_ascii_32_avx2_c32(
+    lup: &[u8],
+    buf: &mut [u64; 4],
+) -> Result<(), EncodeError> {
+    assert_eq!(lup.len(), 32);
+    _binary_to_ascii_32_avx2_c32_chunks32(lup, buf)?;
+    Ok(())
+}
+
+macro_rules! enc_check_error {
+    ($err_buf: expr, $buf_ptr: expr) => {
+        if $err_buf[0] != 0 || $err_buf[1] != 0 || $err_buf[2] != 0 || $err_buf[3] != 0 {
+            // on error
+            for i in 0..4 {
+                let cc = $err_buf[i];
+                for j in 0..8 {
+                    if (cc >> (8 * j)) & 0xFF != 0 {
+                        return Err(EncodeError::InvalidIndex(*($buf_ptr.add(j + 8 * i))));
+                    }
+                }
+            }
+        }
+    };
+}
+
+#[inline(always)]
+pub(crate) unsafe fn _binary_to_ascii_32_avx2_c32_chunks32(
+    lup: &[u8],
+    buf: &mut [u64; 4],
+) -> Result<(), EncodeError> {
+    //panic!("PASS: SSE2");
+    #[cfg(target_arch = "x86")]
+    use core::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use core::arch::x86_64::*;
+    //
+    {
+        let buf_ptr = buf.as_mut_ptr() as *mut u8;
+        //
+        let mm_map01 = _mm_loadu_si128(lup.as_ptr() as *const __m128i);
+        let mm_map02 = _mm_loadu_si128(lup.as_ptr().add(16) as *const __m128i);
+        let mm_map1 = _mm256_broadcastsi128_si256(mm_map01);
+        let mm_map2 = _mm256_broadcastsi128_si256(mm_map02);
+        let err_buf = [0u64; 4];
+        //
+        {
+            // ..|00ffffff|00eeeeee|00dddddd|00cccccc|00bbbbbb|00aaaaaa
+            let mm_in = _mm256_loadu_si256(buf_ptr as *const __m256i);
+            //
+            // check error
+            let mm_err = _mm256_or_si256(
+                _mm256_cmpgt_epi8(_mm256_set1_epi8(0), mm_in),
+                _mm256_cmpgt_epi8(mm_in, _mm256_set1_epi8(32 - 1)),
+            );
+            _mm256_storeu_si256(err_buf.as_ptr() as *mut __m256i, mm_err);
+            enc_check_error!(err_buf, buf_ptr);
+            //_mm_prefetch(buf_ptr.cast::<i8>(), _MM_HINT_T0);
+            let mm_in_p1 = _mm256_add_epi8(mm_in, _mm256_set1_epi8(1));
+            //
+            let mm_mask11 = _mm256_cmpgt_epi8(_mm256_set1_epi8(16), mm_in);
+            let mm_mask22 = _mm256_cmpgt_epi8(_mm256_set1_epi8(16 * 2), mm_in);
+            //
+            let mm_mask1 = mm_mask11;
+            let mm_and1 = _mm256_and_si256(mm_in_p1, mm_mask1);
+            let mm_idx1 = _mm256_sub_epi8(mm_and1, _mm256_set1_epi8(1));
+            let mm_out1 = _mm256_shuffle_epi8(mm_map1, mm_idx1);
+            //
+            let mm_mask2 = _mm256_andnot_si256(mm_mask11, mm_mask22);
+            let mm_and2 = _mm256_and_si256(mm_in_p1, mm_mask2);
+            let mm_idx2 = _mm256_sub_epi8(mm_and2, _mm256_set1_epi8(1));
+            let mm_out2 = _mm256_shuffle_epi8(mm_map2, mm_idx2);
+            //
+            // make out
+            let mm_out = _mm256_or_si256(mm_out1, mm_out2);
+            _mm256_storeu_si256(buf_ptr as *mut __m256i, mm_out);
+        }
+    };
+    //
+    Ok(())
+}
+
+#[cfg(all(test, not(feature = "bench")))]
+mod tests;
+
+#[cfg(all(test, feature = "ubench"))]
+mod benches;
+#[cfg(all(test, feature = "ubench"))]
+#[allow(unused_imports)]
+pub(crate) use benches::*;
---
+++ src/ags/ags_64_avx2/tests.rs
@@ -0,0 +1,67 @@
+#[allow(unused_imports)]
+use super::super::*;
+#[allow(unused_imports)]
+use super::*;
+//use rstest::rstest;
+//use rstest_reuse::{self, *};
+
+fn gen_data_ags_64() -> (AsciiGraphicSet, Vec<u8>, Vec<u8>) {
+    let ags = AsciiGraphicSet::with_slice(&test_utils::_CMAP64);
+    let mut bin_buf = Vec::<u8>::with_capacity(64);
+    for i in 0..64 {
+        bin_buf.push(i);
+    }
+    let mut asc_buf = Vec::<u8>::with_capacity(64);
+    asc_buf.extend_from_slice(&test_utils::_CMAP64);
+    (ags, bin_buf, asc_buf)
+}
+
+#[test]
+fn test_ags_binary_to_ascii_64_avx2_1() {
+    let (ags, bin_buf, asc_buf) = gen_data_ags_64();
+    let mut buf = bin_buf.clone();
+    let r = unsafe { _binary_to_ascii_64_avx2(&ags.binmap, &mut buf) };
+    assert!(r.is_ok());
+    assert_eq!(buf, asc_buf);
+}
+
+#[test]
+fn test_ags_binary_to_ascii_64_avx2_2() {
+    let (ags, bin_buf0, asc_buf0) = gen_data_ags_64();
+    let mut buf = Vec::<u8>::with_capacity(4 * 64);
+    buf.extend_from_slice(&bin_buf0);
+    buf.extend_from_slice(&bin_buf0);
+    buf.extend_from_slice(&bin_buf0);
+    buf.extend_from_slice(&bin_buf0);
+    let r = unsafe { _binary_to_ascii_64_avx2(&ags.binmap, &mut buf) };
+    assert!(r.is_ok());
+    let mut cor = Vec::<u8>::with_capacity(4 * 64);
+    cor.extend_from_slice(&asc_buf0);
+    cor.extend_from_slice(&asc_buf0);
+    cor.extend_from_slice(&asc_buf0);
+    cor.extend_from_slice(&asc_buf0);
+    assert_eq!(buf, cor);
+}
+
+#[test]
+fn test_ags_binary_to_ascii_64_avx2_3() {
+    let (ags, bin_buf0, _asc_buf0) = gen_data_ags_64();
+    for i in 0..64 {
+        let mut buf = bin_buf0.clone();
+        buf.insert(i, 64);
+        let r = unsafe { _binary_to_ascii_64_avx2(&ags.binmap, &mut buf) };
+        assert_eq!(r, Err(EncodeError::InvalidIndex(64)));
+    }
+    for i in 0..64 {
+        let mut buf = bin_buf0.clone();
+        buf.insert(i, 128);
+        let r = unsafe { _binary_to_ascii_64_avx2(&ags.binmap, &mut buf) };
+        assert_eq!(r, Err(EncodeError::InvalidIndex(128)));
+    }
+    for i in 0..64 {
+        let mut buf = bin_buf0.clone();
+        buf.insert(i, 255);
+        let r = unsafe { _binary_to_ascii_64_avx2(&ags.binmap, &mut buf) };
+        assert_eq!(r, Err(EncodeError::InvalidIndex(255)));
+    }
+}
